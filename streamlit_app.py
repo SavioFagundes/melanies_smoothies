6906408cf32a35c2428cf232a0124842506f18da
Savio Fagundes <savio.marques1@aluno.uniplandf.edu.br>
2026-09-01 11:00:52 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,6 @@ else:
     st.success('There are no pending orders right now', icon="👍")
 cnx = st_connection("snowflake")
 session = cnx.session()
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
